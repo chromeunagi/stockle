6604ecaaed48284ab9a6b6d0d75b0d3313f41472
Sam <samgamer95@gmail.com>
2016-03-17 09:06:18 -0700

    added stockle.py

---
+++ stockle.py
@@ -0,0 +1,16 @@
+import os
+
+# TODO
+# 1. command completion
+# 2. 
+#
+
+def main():
+    # set up environment
+    # start up cli
+    return 0
+
+if __name__ == "__main__":
+    # parse command line arguments, flags
+    main()
+
